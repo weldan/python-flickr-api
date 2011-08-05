8bceb458a699a6bdd6109bd55fa18aea77564183
alexis.mignon <alexis.mignon@c04dc755-9e73-fb1f-9025-638fad5d94ba>
2011-08-05 16:57:51 +0000

    Moved FlickrObject to the 'base' module

--- flickr_api/base.py
+++ flickr_api/base.py
@@ -9,6 +9,9 @@ class FlickrAPIError(FlickrError):
         self.message = message
 
 class FlickrDictObject(object):
+    """
+        Tranform recursively JSON dictionnaries into objects
+    """
     def __init__(self,name,obj_dict):
         self.__name__ = name
         for k,v in obj_dict.iteritems() :
@@ -17,3 +20,66 @@ class FlickrDictObject(object):
             if isinstance(v,list) :
                 v = [ FlickrDictObject(k,vi) for vi in v ]
             self.__dict__[k] = v
+
+class FlickrObject(object):
+    """
+        Base Object for Flickr API Objects
+    """
+    __converters__ = []
+    __display__ = []
+    
+    def __init__(self,**params):
+        params["loaded"] = False
+        self._set_properties(**params)
+
+    def _set_properties(self,**params):
+        for c in self.__class__.__converters__ :
+            c(params)
+        self.__dict__.update(params)
+    
+    def __getattr__(self,name):
+        if not self.loaded :
+            self.load()
+        if self.loaded : raise AttributeError("'%s' object has no attribute '%s'"%(self.__class__.__name__,name))
+    
+    def __setattr__(self,name,values):
+        raise RunTimeError("Readonly attribute")
+    
+    def __str__(self):
+        vals = []
+        for k in self.__class__.__display__ :
+            try :
+                value = self.__dict__[k]
+            except KeyError :
+                self.load()
+                value = self.__dict__[k]
+            
+            if isinstance(value,unicode):
+                value = value.encode("utf8")
+            if isinstance(value,str):
+                value = "'%s'"%value
+            else : value = str(value)
+            vals.append("%s = %s"%(k,value))
+        return "%s(%s)"%(self.__class__.__name__,",".join(vals))
+    
+    def __repr__(self): return str(self)
+
+    def getInfo(self):
+        """
+            Returns object information as a dictionnary.
+            Should be overriden.
+        """
+        if not hasattr(self,"id") : raise AttributeError("'%s' object has not attribute '%s'"%(self.__class__.__name__,"id"))
+        return { "loaded" : True}
+
+    def load(self):
+        props = self.getInfo()
+        self._set_properties(**props)
+
+def dict_converter(keys,func):
+    def convert(dict_) :
+        for k in keys :
+            try :
+                dict_[k] = func(dict_[k])
+            except KeyError : pass
+    return convert
