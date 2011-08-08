2ca427910bdffe0501572a3784455a50c27d0e57
alexis.mignon <alexis.mignon@c04dc755-9e73-fb1f-9025-638fad5d94ba>
2011-08-08 14:53:34 +0000

    Added: new api module to perform direct calls to the Flickr API

--- flickr_api/__init__.py
+++ flickr_api/__init__.py
@@ -19,14 +19,11 @@
     email  : alexis.mignon_at_gmail.com
     Date   : 05/08/2011
     
-    You can freely user, modify and redistribute this code for
-    non commercial use. The only requirement is to indicate the
-    copyright information.
-    
 """
 
 from objects import *
 import objects
+import api
 from auth import AuthHandler
 import upload as Upload
 from upload import upload,replace
@@ -34,3 +31,4 @@ from upload import upload,replace
 def set_auth_handler(auth_handler):
     objects.AUTH_HANDLER = auth_handler
     Upload.AUTH_HANDLER = auth_handler
+    api.AUTH_HANDLER = auth_handler
